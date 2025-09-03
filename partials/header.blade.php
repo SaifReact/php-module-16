eb749a43ea704f7f8018aa6b38cc6ab49c5852f4
Md. Saifur Rahman <88313022+SaifReact@users.noreply.github.com>
2025-09-03 23:14:17 +0600

    header.blade.php

--- partials/header.blade.php
+++ partials/header.blade.php
@@ -1,5 +1,28 @@
 <header>
-    <nav>
-        <!-- Paste your nav bar code here -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="{{ url('/') }}">MyPortfolio</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="{{ url('/') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/about') }}">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/portfolio') }}">Portfolio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/contact') }}">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </header>
+
